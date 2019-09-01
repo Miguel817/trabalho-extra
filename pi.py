6cf2e983b542b7484901b5b9995522b083b542ea
miguel817 <herbmi123@gmail.com>
2019-09-01 16:34:23 -0300

    meu primeiro

---
+++ pi.py
@@ -0,0 +1,5 @@
+raio = float(input('valor do raio, para obter a area: '))
+
+area = 3.14 * (raio * raio)
+
+print("A area do circunferencia E: {:.2f}".format(area))
---
+++ valorv.py
@@ -0,0 +1,10 @@
+n=int(input("Infome um valor:"))
+while(n==0):
+	print(" valor invalido:-/")
+	n=int(input("Informe outro valor:"))
+n2=int(input("Informe um novo valor valido:"))
+while(n2==0):
+	print("valor invalido:-/")
+	n2=int(input("informe outro valor:"))
+div=n/n2
+print("O VALOR DA DIVISAO DOS NUMEROS E =",div)
